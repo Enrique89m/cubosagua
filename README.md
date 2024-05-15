bfaf9d2056c74bc97175f2c1036721aca890281f
Enrique <enrmarore@alu.edu.gva.es>
2024-05-16 00:45:56 +0200

    V1 - 16/05/2024 / 00:41 A.M Se añade una nueva opción (M) para llenar todos los cubos de una.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MAFF
+# EMO
 
 CFGS Desenrotllament d'Aplicacions Web
 
@@ -6,5 +6,8 @@ Entorns de Desenrotllament
 
 ## Programa del joc dels poals
 
-Programa exemple per activitat de Github amb Python
+## Versión 1 --- 16/05/2024 / 00:41 A.M
 
+---
+
+> Se añade nueva opción (T) en el menú para llenar todos los cubos de una.
--- cubosagua.py
+++ cubosagua.py
@@ -63,6 +63,7 @@ class juegoCubos:
         print('  (L)lenar un cubo')
         print('  (V)aciar un cubo')
         print('  (M)over el agua de un cubo a otro')
+        print('  (T)Llenar todos los cubos')
         print('  (S)alir')
 
         while True:
@@ -71,7 +72,7 @@ class juegoCubos:
                 print('Gracias por jugar!')
                 sys.exit()
 
-            if move in ('L', 'V', 'M'):
+            if move in ('L', 'V', 'M', 'T'):
                 return move
 
     def selecCubo(self, mensaje):
@@ -104,6 +105,12 @@ class juegoCubos:
         self.aguaEnCubos[cuboDestino] += cantidadAMover
         self.pasos += 1
 
+    def llenarTodosCubos(self):
+
+        for cubo in self.aguaEnCubos:
+            self.aguaEnCubos[cubo] = int(cubo)
+        self.pasos += 1
+
     def jugar(self):
         self.mostrarEstadoCubos()
         while True:
@@ -118,8 +125,11 @@ class juegoCubos:
                 cuboOrigen = self.selecCubo('Selecciona el cubo ORIGEN 8, 5, 3 o SALIR:')
                 cuboDestino = self.selecCubo('Selecciona el cubo DESTINO 8, 5, 3 o SALIR:')
                 self.moverCubo(cuboOrigen, cuboDestino)
+            elif opcion == "T":
+                self.llenarTodosCubos()
             self.mostrarEstadoCubos()
             self.checkObjetivo()
+
 
 
 if __name__ == "__main__":
